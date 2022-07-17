c6a6de35987fa8953f8a4f20f1770bdcb9e63d8b
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2022-07-17 18:45:03 +0500

    adds css fix for ios

--- web/src/components/Homepage/PromoSection.js
+++ web/src/components/Homepage/PromoSection.js
@@ -122,7 +122,7 @@ const Wrapper = styled.div`
       width: 100%;
       height: 100vh;
       z-index: 0;
-      transform: translate3d(0, 0, 0);
+      -webkit-transform: translate3d(0, 0, 0);
       -webkit-transform: translateZ(0);
     }
   }
