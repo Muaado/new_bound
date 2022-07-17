6ff8a8c2a73ed8fab6f6990884a955aa902d9cfc
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2022-07-17 17:51:52 +0500

    adds transform property

--- web/src/components/Homepage/PromoSection.js
+++ web/src/components/Homepage/PromoSection.js
@@ -121,8 +121,9 @@ const Wrapper = styled.div`
       background-size: cover;
       width: 100%;
       height: 100vh;
-      z-index: 0;
-      -webkit-transform: translateZ(0);
+      z-index: -1;
+      transform: translate3d(0, 0, 0);
+      -webkit-transform: translateZ(-1);
     }
   }
 `;
